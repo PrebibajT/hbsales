82b9859c19272ba4b90649aadad6ca147a498489
PrebibajT <53578635+PrebibajT@users.noreply.github.com>
2020-06-30 14:24:58 -0300

    Correção

--- src/main/java/br/com/hbsis/fornecedor/IFornecedorRepository.java
+++ src/main/java/br/com/hbsis/fornecedor/IFornecedorRepository.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-/**
- * Classe responsável pela comunciação com o banco de dados
- */
 @Repository
 interface IFornecedorRepository extends JpaRepository<Fornecedor, Long> {
 
