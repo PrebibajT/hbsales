e522b90fc3ca8da4caa924200130822098b2e82c
PrebibajT <53578635+PrebibajT@users.noreply.github.com>
2020-06-30 15:42:47 -0300

    Finalização

--- src/main/java/br/com/hbsis/usuario/IUsuarioRepository.java
+++ src/main/java/br/com/hbsis/usuario/IUsuarioRepository.java
@@ -3,9 +3,7 @@ package br.com.hbsis.usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Classe responsável pela comunciação com o banco de dados
- */
 @Repository
 interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
+  
 }
